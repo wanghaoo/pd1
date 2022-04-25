7a75d57e07c708ade8588d9c4180471f9aa37c80
王宇 <wang.yu.lion@gmail.com>
2022-04-26 01:02:36 +0800

    add

--- contracts/PD1Certificate.sol
+++ contracts/PD1Certificate.sol
@@ -21,18 +21,18 @@ contract PD1Certificate is Context {
 
   event ChangeHolder(address contractAddress, address holderAddress, uint256 tokenId);
 
-  modifier Owner721Token(address contractAddress, uint256 tokenId) {
+  modifier Owner721Token(address contractAddress) {
     require(contractAddress != address(0), "zero contract address");
     require(contractAddress.isContract(), "invalid contract address");
     _;
   }
 
-  function getTokenOwner(address payable contractAddress, uint256 tokenId) public payable returns(address) {
+  function getTokenOwner(address payable contractAddress, uint256 tokenId) public view returns(address) {
     return IERC721(contractAddress).ownerOf(tokenId);
   }
 
   function issuingOfCertificates(address contractAddress, address holderAddress, uint256 tokenId) 
-  public Owner721Token(contractAddress, tokenId) {
+  public Owner721Token(contractAddress) {
     require(holderAddress != address(0), "zero holder address");
     require(holderAddress != _msgSender(), "owner not be holder");
     _certificateHolder[contractAddress][tokenId].holder = holderAddress;
@@ -41,20 +41,29 @@ contract PD1Certificate is Context {
   }
 
   function clearCertificatesHolder(address contractAddress, uint256 tokenId) 
-  public Owner721Token(contractAddress, tokenId) {
+  public Owner721Token(contractAddress) {
     require(ownerOf(contractAddress, tokenId) == _msgSender(), "sender not owner");
     _certificateHolder[contractAddress][tokenId].holder = address(0);
     emit ChangeHolder(contractAddress, address(0), tokenId);
   }
 
   function ownerOf(address contractAddress, uint256 tokenId) 
-  public view Owner721Token(contractAddress, tokenId) returns(address) {
+  public view Owner721Token(contractAddress) returns(address) {
     return _certificateHolder[contractAddress][tokenId].tokenOwner;
   }
 
-  function holdOf(address contractAddress, uint256 tokenId) 
-  public view Owner721Token(contractAddress, tokenId) returns(address) {
+  function holdOf(address contractAddress, uint256 tokenId)
+  public view Owner721Token(contractAddress) returns(address) {
     return _certificateHolder[contractAddress][tokenId].holder;
   }
 
+  function checkHolder(address contractAddress, address holderAddress, uint256 tokenId) 
+  public view Owner721Token(contractAddress) returns (bool) {
+    require(holderAddress != address(0), "zero holder address");
+    address owner = _certificateHolder[contractAddress][tokenId].tokenOwner;
+
+    require(owner == _msgSender(), "sender not owner");
+
+    return _certificateHolder[contractAddress][tokenId].holder == holderAddress;
+  }
 }
