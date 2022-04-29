fba5cd8d0ae2dcad66f5b107125691039905d248
王宇 <wang.yu.lion@gmail.com>
2022-04-29 15:56:13 +0800

    add

--- contracts/PD1Permission.sol
+++ contracts/PD1Permission.sol
@@ -2,17 +2,32 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Arrays.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract PD1Permission is ERC721URIStorage {
     using Counters for Counters.Counter;
+    using Strings for uint256;
+
     Counters.Counter private _tokenIds;
 
-    mapping(address => mapping(address => mapping(uint256 => Permission))) _grantPermission;
-    struct Permission {
-      uint256 permissionTag;
-      string reason;
+    Counters.Counter private _permissionPrimaryKey;
+
+    mapping (bytes32 => uint256[]) _ownerTokenIndex;
+    mapping (bytes32 => uint256[]) _grantUserTokenIndex;
+    mapping (bytes32 => uint256) _grantUserTokenPermissionIndex;
+
+    Permission[] _permissionTable;
+    struct Permission{
+      address owner;
+      address grantUser;
+      uint256 tokenId;
+      uint256 permission;
+      string resion;
     }
+
+    mapping(address => uint256) _grantTokenUsers; // token -> 授权用户
 
     event PermissionChange(address indexed _owner, address indexed _user, uint256 indexed _tokenId, uint256 permission, string reason);
 
@@ -37,70 +52,143 @@ contract PD1Permission is ERC721URIStorage {
     }
 
     /**
-     * @dev Return if "permissionTag" is the permission granted to the user by the Token owner
+     * @dev Return if "_permission" is the permission granted to the user by the Token owner
      * @param _user granted user address
      * @param _tokenId granted token id
-     * @param _permissionTag token permission tag
+     * @param _permission token permission tag
      */
-    function access(address _user, uint256 _tokenId, uint256 _permissionTag) 
+    function access(address _user, uint256 _tokenId, uint256 _permission)
     external ZeroAddress(_user) view returns (bool) {
-      require(_isApprovedOrOwner(_msgSender(), _tokenId), "gran caller is not owner nor approved");
-      require(_permissionTag > 0, "permission tag not be zero");
+      require(_isApprovedOrOwner(_msgSender(), _tokenId), "grant caller is not owner nor approved");
+      require(_permission > 0, "permission tag not be zero");
+      
+      bytes32 grantUserTokenPermissionKey = _buildGrantUserTokenPermissionIndex(_msgSender(), _tokenId, _user, _permission);
+      if (_grantUserTokenPermissionIndex[grantUserTokenPermissionKey] == 0) {
+        return false;
+      }
 
-      return _grantPermission[_msgSender()][_user][_tokenId].permissionTag == _permissionTag;
+      uint256 index = _grantUserTokenPermissionIndex[grantUserTokenPermissionKey];
+      Permission memory permission_ = _permissionTable[index];
+
+      return _user == permission_.grantUser && _tokenId == permission_.tokenId && _permission == permission_.permission;
     }
 
     /**
-     * @dev Return "permissionTag" is the permission granted to the user by the Token owner
+     * @dev Return "permissions" is the permission granted to the user by the Token owner
      * @param _user granted user address
      * @param _tokenId granted token id
      */
     function permission(address _user, uint256 _tokenId) 
-    external ZeroAddress(_user) view returns (uint256) {
-      require(_isApprovedOrOwner(_msgSender(), _tokenId), "gran caller is not owner nor approved");
+    external ZeroAddress(_user) view returns (uint256[] memory) {
+      require(_isApprovedOrOwner(_msgSender(), _tokenId), "grant caller is not owner nor approved");
 
-      return _grantPermission[_msgSender()][_user][_tokenId].permissionTag;
+      bytes32 key = _buildGrantUserTokenIndexKey(_msgSender(), _tokenId, _user);
+      if (_grantUserTokenIndex[key].length <= 0) {
+        return new uint256[](0);
+      }
+
+      uint256[] memory indexes = _grantUserTokenIndex[key];
+
+      uint256[] memory result = new uint256[](indexes.length);
+      for (uint256 i = 0; i < indexes.length; i ++) {
+        Permission memory permission_ = _permissionTable[indexes[i]];
+        result[i] = permission_.permission;
+      }
+      return result;
     }
 
 
     /**
-     * @dev Grant "_tokenId" "_permisstionTag" to the "_to" address
+     * @dev Grant "_tokenId" "_permission" to the "_to" address
      * @param _to granted to user address
      * @param _tokenId granted token id
-     * @param _permissionTag token permission tag
+     * @param _permission token permission tag
      * @param _reason grant reason
      */
-    function grant(address _to, uint256 _tokenId, uint256 _permissionTag, string memory _reason) 
+    function grant(address _to, uint256 _tokenId, uint256 _permission, string memory _reason) 
     external ZeroAddress(_to) payable {
-      require(_isApprovedOrOwner(_msgSender(), _tokenId), "gran caller is not owner nor approved");
-      require(_permissionTag > 0, "permission tag not be zero");
+      require(_isApprovedOrOwner(_msgSender(), _tokenId), "grant caller is not owner nor approved");
+      require(_permission > 0, "permission tag not be zero");
 
-      _grantPermission[_msgSender()][_to][_tokenId].permissionTag = _permissionTag;
-      _grantPermission[_msgSender()][_to][_tokenId].reason = _reason;
+      // build access index
+      bytes32 _grantUserTokenPermissionIndexKey = _buildGrantUserTokenPermissionIndex(_msgSender(), _tokenId, _to, _permission);
+      require(_grantUserTokenPermissionIndex[_grantUserTokenPermissionIndexKey] == 0, "user token permission existend");
 
-      emit PermissionChange(_msgSender(), _to, _tokenId, _permissionTag, _reason);
+      // index permission
+      _permissionPrimaryKey.increment();
+      uint256 index = _permissionPrimaryKey.current();
+
+      _grantUserTokenPermissionIndex[_grantUserTokenPermissionIndexKey] = index;
+
+      _permissionTable[index] = Permission(_msgSender(), _to, _tokenId, _permission, _reason);
+
+      //build clear permission index
+      bytes32 _ownerTokenIndexKey = _buildOwnerTokenIndexKey(_msgSender(), _tokenId);
+      _ownerTokenIndex[_ownerTokenIndexKey].push(index);
+
+      // build permission index
+      bytes32 _grantUserTokenIndexKey = _buildGrantUserTokenIndexKey(_msgSender(), _tokenId, _to);
+      _grantUserTokenIndex[_grantUserTokenIndexKey].push(index);
+
+      emit PermissionChange(_msgSender(), _to, _tokenId, _permission, _reason);
     }
 
     /**
-     * @dev Revoke "_to" user "_tokenId" tonken permission
+     * @dev Revoke "_to" user "_tokenId" permission
      * @param _to revoke user address
      * @param _tokenId revoke token id
      */
     function revoke(address _to, uint256 _tokenId)
-    external payable {
-      _revoke(_to, _tokenId);
-    }
-
-    function _revoke(address _to, uint256 _tokenId)
-    private ZeroAddress(_to) {
+    external ZeroAddress(_to) payable {
       require(_isApprovedOrOwner(_msgSender(), _tokenId), "gran caller is not owner nor approved");
 
-      if (_grantPermission[_msgSender()][_to][_tokenId].permissionTag > 0) {
-        _grantPermission[_msgSender()][_to][_tokenId].permissionTag = 0;
-        _grantPermission[_msgSender()][_to][_tokenId].reason = "";
+      bytes32 _ownerTokenIndexKey =  _buildOwnerTokenIndexKey(_msgSender(), _tokenId);
+      require(_ownerTokenIndex[_ownerTokenIndexKey].length > 0, "grant user token nonexistend");
 
-        emit PermissionChange(_msgSender(), _to, _tokenId, 0, "");
+      uint256[] memory permissionTableIndex = _ownerTokenIndex[_ownerTokenIndexKey];
+
+      for (uint256 i = 0; i < permissionTableIndex.length; i ++) {
+        uint256 _permission = _permissionTable[permissionTableIndex[i]].permission;
+        delete _permissionTable[permissionTableIndex[i]];
+        _removeGrantUserTokenPermissionIndex(_msgSender(), _tokenId, _to, _permission);
       }
+      _removeOwnerTokenIndex(_msgSender(), _tokenId);
+      _removeGrantUserTokenIndex(_msgSender(), _tokenId, _to);
+
+      emit PermissionChange(_msgSender(), _to, _tokenId, 0, "");
+    }
+
+    /**
+     * @dev clear permission when transfer
+     */
+    function _clearTokenPermission(address _from, uint256 _tokenId)
+    private {
+      bytes32 _ownerTokenIndexKey =  _buildOwnerTokenIndexKey(_msgSender(), _tokenId);
+      if (_ownerTokenIndex[_ownerTokenIndexKey].length <= 0) {
+        return;
+      }
+
+      uint256[] memory permissionTableIndex = _ownerTokenIndex[_ownerTokenIndexKey];
+
+      for (uint256 i = 0; i < permissionTableIndex.length; i ++) {
+        Permission memory _permission = _permissionTable[permissionTableIndex[i]];
+
+        delete _permissionTable[permissionTableIndex[i]];
+        _removeGrantUserTokenIndex(_from, _tokenId, _permission.grantUser);
+        _removeGrantUserTokenPermissionIndex(_from, _tokenId, _permission.grantUser, _permission.permission);
+
+        emit PermissionChange(_from, _permission.grantUser, _tokenId, 0, "");
+      }
+      _removeOwnerTokenIndex(_from, _tokenId);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override(ERC721) {
+      super.safeTransferFrom(from, to, tokenId);
+      _clearTokenPermission(from, tokenId);
     }
 
     function safeTransferFrom(
@@ -108,17 +196,44 @@ contract PD1Permission is ERC721URIStorage {
         address to,
         uint256 tokenId,
         bytes calldata data
-    ) public override {
+    ) public override(ERC721) {
       super.safeTransferFrom(from, to, tokenId, data);
-      _revoke(to, tokenId);
+      _clearTokenPermission(from, tokenId);
     }
 
     function transferFrom(
         address from,
         address to,
         uint256 tokenId
-    ) public override {
-      super.safeTransferFrom(from, to, tokenId);
-      _revoke(to, tokenId);
+    ) public override(ERC721) {
+      super.transferFrom(from, to, tokenId);
+      _clearTokenPermission(from, tokenId);
+    }
+
+    function _buildOwnerTokenIndexKey(address _owner, uint256 _tokenId) 
+    internal pure returns(bytes32) {
+      return keccak256(abi.encodePacked(_owner, "::", _tokenId.toString()));
+    }
+
+    function _buildGrantUserTokenIndexKey(address _owner, uint256 _tokenId, address _to) 
+    internal pure returns(bytes32) {
+      return keccak256(abi.encodePacked(_owner, "::", _tokenId.toString(), "::", _to));
+    }
+
+    function _buildGrantUserTokenPermissionIndex(address _owner, uint256 _tokenId, address _to, uint256 _permission) 
+    internal pure returns(bytes32) {
+      return keccak256(abi.encodePacked(_owner, "::", _tokenId.toString(), "::", _to, "::", _permission));
+    }
+
+    function _removeOwnerTokenIndex(address _owner, uint256 _tokenId) internal {
+      delete _ownerTokenIndex[_buildOwnerTokenIndexKey(_owner, _tokenId)];
+    }
+
+    function _removeGrantUserTokenIndex(address _owner, uint256 _tokenId, address _to) internal {
+      delete _grantUserTokenIndex[_buildGrantUserTokenIndexKey(_owner, _tokenId, _to)];
+    }
+
+    function _removeGrantUserTokenPermissionIndex(address _owner, uint256 _tokenId, address _to, uint256 _permission) internal {
+      delete _grantUserTokenPermissionIndex[_buildGrantUserTokenPermissionIndex(_owner, _tokenId, _to, _permission)];
     }
 }
